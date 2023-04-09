6fc054fd5a9ba551657b291a0574688031ca2035
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-04-09 21:12:30 +0200

    setup vite + react + firebase config

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Fitness Davide Ghiotto</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Rep count: {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
