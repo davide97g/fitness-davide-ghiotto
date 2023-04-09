bf06a0cd6c5f7e00a45e41de87d69c51eb5c0247
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-04-09 21:25:25 +0200

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,79 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      mode: "production",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Personal Finance",
+        short_name: "Personal Finance",
+        theme_color: "#eaefea",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#eaefea",
+        icons: [
+          {
+            src: "/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/img/icons/apple-touch-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/apple-touch-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/apple-touch-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/apple-touch-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf}"],
+      },
+    }),
+  ],
+});
