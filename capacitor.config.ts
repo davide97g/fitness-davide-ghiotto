6ffad7b2f7d9062cbdd540d30ef961cb382fb201
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-08-29 17:09:50 +0200

    capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.ghiotto.davidenko',
+	appName: 'fitness-davide-ghiotto',
+	webDir: 'dist',
+	server: {
+		androidScheme: 'https',
+	},
+};
+
+export default config;
